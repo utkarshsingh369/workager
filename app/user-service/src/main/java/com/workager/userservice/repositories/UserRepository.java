40cb9c1da4c84c4c6a470e5e81169257c947c908
Utkarsh Singh <utkarsh060902@gmail.com>
2024-10-27 10:06:55 +0530

    setup(user,lib): spring data jpa for user

---
+++ app/user-service/src/main/java/com/workager/userservice/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.workager.userservice.repositories;
+
+import com.workager.lib.entities.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {}
